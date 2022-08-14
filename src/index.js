ac3441792352b67a8d63d8c11374e3bd494ca7dd
Nguyễn Tuấn Anh <77733068+kurei01@users.noreply.github.com>
2022-08-14 15:28:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,29 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { legacy_createStore as createStore, combineReducers } from "redux";
-import { Provider } from "react-redux";
-import questionReducer from "./testingOnline/redux/questionReducer";
-import answerReducer from "./testingOnline/redux/answerReducer";
-
-const rootReducer = combineReducers({
-  //khai báo reducers
-  //key: reducer
-  question: questionReducer,
-  answer: answerReducer,
-});
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
